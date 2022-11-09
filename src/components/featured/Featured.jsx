787aaaf5944e7f50919ef807b2366a6d74a4d5e9
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-11-09 22:51:38 +0500

    featured page updated

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,48 +1,71 @@
 import { InfoOutlined, PlayArrow } from '@mui/icons-material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 import './featured.scss'
 
-const Featured = (props) => {
-  return (
-    <>
-    <div className="featured">
-        <img src="https://images.pexels.com/photos/3406020/pexels-photo-3406020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-        <div className="featuredContainer">
-            {props.type && (
-                <div className='category'>
-                <span>{props.type === 'movie' ? "Movies" : "Series"}</span>
-                <select className="genre">
-                    <option value="Genre">Genre</option>
-                    <option value="Animation">Animation</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Crime">Crime</option>
-                    <option value="Drama">Drama</option>
-                    <option value="Experimental">Experimental</option>
-                    <option value="Fantasy">Fantasy</option>
-                    <option value="Historical">Historical</option>
-                    <option value="Horror">Horror</option>
-                    <option value="Romance">Romance</option>
-                </select>
-            </div>
-            )}
-            <div className="info">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/d/df/Ozark_TV_series_logo.png?20190409070319" alt="" />
-            </div>
-            <span className="desc">
-            Created by Bill Dubuque ("The Accountant," "The Judge"), this drama series stars Jason Bateman as Marty Byrde, a financial planner who relocates his family from Chicago to a summer resort community in the Ozarks.With wife Wendy and their two kids in tow, Marty is on the move after a money-laundering scheme goes wrong 
-            </span>
-            <div className="buttons">
-                <button className="play">
-                    <PlayArrow/>
-                    <span>Play</span>
-                </button>
-                <button className="more">
-                    <InfoOutlined/>
-                    <span>Info</span>
-                </button>
+const Featured = ({type}) => {
+    const [content, setContent] = useState({});
+
+    useEffect(()=>{
+        const getRandomContent = async () =>{
+            try{
+                const res = await axios.get(
+                    `/movies/random?type=${type}`,                                             //Fetching the proxy+(given url) to get the movie of required type. 
+                    {                                                                        
+                        headers: {                                                             //We also needed authentication so we are using headers to  send the required token to authenticate the user and fetch the  lists data.
+                          token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDNmYWRmMDNlYTEzMWY4OWFmYThiOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NzkzMDQ3NywiZXhwIjoxNjY4MzYyNDc3fQ.GwaPkwsBIdxbunULrPTdC5PFO5ucyhgqHPBNGkdPMWA"
+                        },
+                    }
+                ) 
+                setContent(res.data[0])
+            }
+            catch(err){
+                console.log(err)
+            }
+        };
+        getRandomContent();
+    }, [type]);   //here type means that when the type will be changed then the useEffect will be triggered.
+
+    return (
+        <>
+        <div className="featured">
+            <img src={content.img} alt="" />
+            <div className="featuredContainer">
+                {type && (
+                    <div className='category'>
+                        <span>{type === 'movie' ? "Movies" : "Series"}</span>
+                        <select className="genre">
+                            <option value="Genre">Genre</option>
+                            <option value="Animation">Animation</option>
+                            <option value="Comedy">Comedy</option>
+                            <option value="Crime">Crime</option>
+                            <option value="Drama">Drama</option>
+                            <option value="Experimental">Experimental</option>
+                            <option value="Fantasy">Fantasy</option>
+                            <option value="Historical">Historical</option>
+                            <option value="Horror">Horror</option>
+                            <option value="Romance">Romance</option>
+                        </select>
+                    </div>
+                )}
+                <div className="info">
+                    <img src={content.imgTitle} alt="" />
+                </div>
+                <span className="desc">
+                    {content.desc}
+                </span>
+                <div className="buttons">
+                    <button className="play">
+                        <PlayArrow/>
+                        <span>Play</span>
+                    </button>
+                    <button className="more">
+                        <InfoOutlined/>
+                        <span>Info</span>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
     </>
   )
 }
